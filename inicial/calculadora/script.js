0515b806072075f1e5b70a0d82f14333024e8307
rafaeldias34571-dot <rafaeldias34571@gmail.com>
2026-01-14 21:17:20 -0300

    Add files via upload

--- inicial/calculadora/script.js
+++ inicial/calculadora/script.js
@@ -2,8 +2,6 @@ function soma(){
     var numero1 = parseFloat(document.getElementById("n1").value)
     var numero2 = parseFloat(document.getElementById("n2").value)
     var resp = document.getElementById("resp")
-    // nao pode deixar console log nos codigos
-    // console.log(typeof numero1, typeof numero2)
     resp.textContent = numero1 + numero2
 }
 
